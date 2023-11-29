611409bc01b3ff4a9f28855a2311e1670aeab03e
Mohit Goyal <mohit.goyal@drdroid.io>
2023-11-29 19:53:34 +0530

    adds exception handling

--- env_vars.py
+++ env_vars.py
@@ -6,7 +6,12 @@ SLACK_REDIRECT_URI = 'your_redirect_uri'
 # Replace 'YOUR_SLACK_VERIFICATION_TOKEN' with your Slack app's verification token
 SLACK_VERIFICATION_TOKEN = 'your_verification_token'
 
-AWS_ACCESS_KEY = 'your_aws_access_key'
-AWS_SECRET_KEY = 'your_aws_secret_key'
-S3_BUCKET_NAME = 'your_s3_bucket_name'
+PUSH_TO_LOCAL_DB = False
+
 PUSH_TO_S3 = True
+AWS_ACCESS_KEY = 'your_access_key'
+AWS_SECRET_KEY = 'your_secret_key'
+S3_BUCKET_NAME = 'your_bucket_name'
+
+PUSH_TO_SLACK = True
+SLACK_URL = 'your_slack_url'
