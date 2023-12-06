8222b50fc54f9f250f3ac07e2ab490b779177056
Mohit Goyal <mohit.goyal@drdroid.io>
2023-12-06 13:40:16 +0530

    updates persistance layer access

--- persistance/db_utils.py
+++ persistance/db_utils.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_slack_workspace_config_by(team_id: str, bot_user_id: str = None, bot_auth_token: str = None,
-                                  team_name: str = None, is_active: bool = None, get_all_workspaces=False):
+                                  team_name: str = None, is_active: bool = None):
     """
     Fetch a SlackWorkspaceConfig row based on different options.
     """
@@ -21,23 +21,26 @@ def get_slack_workspace_config_by(team_id: str, bot_user_id: str = None, bot_aut
         filters['bot_auth_token'] = bot_auth_token
     if is_active is not None:
         filters['is_active'] = is_active
-    if get_all_workspaces:
-        slack_workspace_config = SlackWorkspaceConfig.query.filter_by(**filters).all()
-    else:
-        slack_workspace_config = SlackWorkspaceConfig.query.filter_by(**filters).first()
-    return slack_workspace_config
+    return SlackWorkspaceConfig.query.filter_by(**filters).all()
 
 
 def create_slack_workspace_config(team_id: str, bot_user_id: str, bot_auth_token: str, team_name: str = None,
                                   should_update=True):
     try:
-        slack_workspace_config = get_slack_workspace_config_by(team_id=team_id, bot_user_id=bot_user_id,
-                                                               bot_auth_token=bot_auth_token)
-        if slack_workspace_config:
+        slack_workspace_configs = get_slack_workspace_config_by(team_id=team_id, bot_user_id=bot_user_id,
+                                                                bot_auth_token=bot_auth_token)
+        if slack_workspace_configs:
+            slack_workspace_config = slack_workspace_configs[0]
             if not should_update:
                 return slack_workspace_config, False
             else:
-                return update_slack_workspace_config(team_id, bot_user_id, bot_auth_token, team_name), True
+                updated_slack_workspace_config = update_slack_workspace_config(slack_workspace_config,
+                                                                               team_name,
+                                                                               True)
+                if updated_slack_workspace_config:
+                    return updated_slack_workspace_config, True
+                else:
+                    return None, False
         new_slack_workspace_config = SlackWorkspaceConfig(team_id=team_id,
                                                           team_name=team_name,
                                                           bot_user_id=bot_user_id,
@@ -51,29 +54,28 @@ def create_slack_workspace_config(team_id: str, bot_user_id: str, bot_auth_token
         return None
 
 
-def update_slack_workspace_config(team_id: str, bot_user_id: str, bot_auth_token: str, team_name: str = None):
+def update_slack_workspace_config(slack_workspace_config: SlackWorkspaceConfig, team_name: str = None,
+                                  is_active: bool = None):
     """
     Update an existing SlackWorkspaceConfig instance in the database.
     """
     try:
-        slack_workspace_config = get_slack_workspace_config_by(team_id, bot_user_id, bot_auth_token, team_name)
-
-        if slack_workspace_config:
-            slack_workspace_config.team_id = team_id
-            slack_workspace_config.team_name = team_name
-            slack_workspace_config.bot_user_id = bot_user_id
-            slack_workspace_config.bot_auth_token = bot_auth_token
-            db.session.commit()
-            return slack_workspace_config
-        else:
+        if not slack_workspace_config:
+            logger.error(f"Error while updating SlackWorkspaceConfig: with error: slack_workspace_config is None")
             return None
+        if team_name:
+            slack_workspace_config.team_name = team_name
+        if is_active is not None:
+            slack_workspace_config.is_active = is_active
+        db.session.commit()
+        return slack_workspace_config
     except Exception as e:
-        logger.error(f"Error while updating SlackWorkspaceConfig: {team_id}:{team_name} with error: {e}")
+        logger.error(f"Error while updating SlackWorkspaceConfig {slack_workspace_config.team_id} with error: {e}")
         db.session.rollback()
         return None
 
 
-def get_slack_bot_config_by_id(slack_workspace_id: str, channel_id: str, is_active: bool = None):
+def get_slack_bot_configs_by(slack_workspace_id: str = None, channel_id: str = None, is_active: bool = None):
     """
     Fetch a SlackBotConfig row based on the bot_config_id.
     """
@@ -85,9 +87,7 @@ def get_slack_bot_config_by_id(slack_workspace_id: str, channel_id: str, is_acti
     if is_active is not None:
         filters['is_active'] = is_active
 
-    slack_bot_config = SlackBotConfig.query.filter_by(**filters).first()
-
-    return slack_bot_config
+    return SlackBotConfig.query.filter_by(**filters).all()
 
 
 def create_slack_bot_config(slack_workspace_id, channel_id, event_ts, channel_name=None):
@@ -95,10 +95,11 @@ def create_slack_bot_config(slack_workspace_id, channel_id, event_ts, channel_na
     Create a new SlackBotConfig instance and add it to the database.
     """
     try:
-        slack_bot_config = get_slack_bot_config_by_id(slack_workspace_id, channel_id)
-        if slack_bot_config:
-            if not slack_bot_config.is_active:
-                updated_slack_bot_config = update_slack_bot_config(slack_bot_config.id, channel_id, event_ts, True)
+        slack_bot_configs = get_slack_bot_configs_by(slack_workspace_id, channel_id)
+        if slack_bot_configs:
+            slack_bot_config = slack_bot_configs[0]
+            if slack_bot_config and not slack_bot_config.is_active:
+                updated_slack_bot_config = update_slack_bot_config(slack_bot_config, is_active=True)
                 if updated_slack_bot_config:
                     return updated_slack_bot_config, True
                 else:
@@ -109,7 +110,8 @@ def create_slack_bot_config(slack_workspace_id, channel_id, event_ts, channel_na
             slack_workspace_id=slack_workspace_id,
             channel_id=channel_id,
             channel_name=channel_name,
-            event_ts=event_ts
+            event_ts=event_ts,
+            is_active=True
         )
 
         db.session.add(new_slack_bot_config)
@@ -121,24 +123,24 @@ def create_slack_bot_config(slack_workspace_id, channel_id, event_ts, channel_na
         return None, False
 
 
-def update_slack_bot_config(bot_config_id, channel_id, event_ts, is_active):
+def update_slack_bot_config(slack_bot_config: SlackBotConfig, event_ts: str = None, channel_name: str = None,
+                            is_active: bool = None):
     """
     Update an existing SlackBotConfig instance in the database.
     """
     try:
-        slack_bot_config = SlackBotConfig.query.get(bot_config_id)
-
-        if slack_bot_config:
-            slack_bot_config.channel_id = channel_id
-            slack_bot_config.event_ts = event_ts
-            slack_bot_config.is_active = is_active
-
-            db.session.commit()
-
-            return slack_bot_config
-        else:
+        if not slack_bot_config:
+            logger.error(f"Error while updating SlackBotConfig: with error: slack_bot_config is None")
             return None
+        if channel_name:
+            slack_bot_config.channel_name = channel_name
+        if event_ts:
+            slack_bot_config.event_ts = event_ts
+        if is_active is not None:
+            slack_bot_config.is_active = is_active
+        db.session.commit()
+        return slack_bot_config
     except Exception as e:
-        logger.error(f"Error while updating SlackBotConfig: {bot_config_id} with error: {e}")
+        logger.error(f"Error while updating SlackBotConfig: {slack_bot_config.id} with error: {e}")
         db.session.rollback()
         return None
