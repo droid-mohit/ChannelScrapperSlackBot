a839f82ed4aa379c992cd5b02a738780a5c819e6
Mohit Goyal <mohit.goyal@drdroid.io>
2023-12-14 19:55:28 +0530

    adds new env vars

--- env_vars.py
+++ env_vars.py
@@ -1,3 +1,6 @@
+# Flask APP Configurations
+FLAKS_APP_SECRET_KEY = 'secret'
+
 # Replace these with your app's information
 SLACK_APP_ID = 'your_app_id'
 SLACK_CLIENT_ID = 'your_client_id'
@@ -11,7 +14,6 @@ AWS_SECRET_KEY = 'your_aws_secret_key'
 METADATA_S3_BUCKET_NAME = 'your_metadata_s3_bucket_name'
 RAW_DATA_S3_BUCKET_NAME = 'your_raw_data_s3_bucket_name'
 
-# Slack Notification Credentials
 PUSH_TO_SLACK = True
 SLACK_URL = 'your_slack_webhook_url'
 
@@ -20,3 +22,7 @@ PG_DB_HOSTNAME = 'localhost'
 PG_DB_USERNAME = ''
 PG_DB_PASSWORD = ''
 PG_DB_NAME = 'data_scrapper_db'
+
+# G-chat App Configurations
+GOOGLE_OAUTH_REDIRECT_URI = 'your_redirect_uri'
+GOOGLE_CLIENT_SECRETS_FILE = "google_chat_app_secret.json"
