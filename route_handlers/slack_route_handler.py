687a6041c9f3a50560b5fc97d60f7b8c31403382
Mohit Goyal <mohit.goyal@drdroid.io>
2023-12-19 19:26:58 +0530

    minor bug fix

--- route_handlers/slack_route_handler.py
+++ route_handlers/slack_route_handler.py
@@ -117,8 +117,7 @@ def handle_event_callback(data: Dict):
                                            + " " + "channel: " + "*" + channel_header + " and channel id: " + "*" + \
                                            channel_id + "*" + " at " + "event_ts: " + event_ts
                             publish_message_to_slack(message_text)
-                            current_time = get_current_time()
-                            data_fetch_job.delay(bot_auth_token, channel_id, event_ts, current_time, '')
+                            data_fetch_job.delay(bot_auth_token, channel_id, event_ts, '')
                     return True
                 else:
                     logger.error(f"Error while saving SlackBotConfig for workspace: {team_id}:{channel_id}:{event_ts}")
